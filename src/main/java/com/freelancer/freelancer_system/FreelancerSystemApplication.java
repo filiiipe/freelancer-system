cd29fa3d6d303237fa501ae819d0072e5b6cd5f3
Filipe Araujo <luiz4r4ujo@gmail.com>
2025-01-05 18:12:36 -0300

    Initial commit

---
+++ src/main/java/com/freelancer/freelancer_system/FreelancerSystemApplication.java
@@ -0,0 +1,13 @@
+package com.freelancer.freelancer_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelancerSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelancerSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/freelancer/freelancer_system/FreelancerSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freelancer.freelancer_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelancerSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
